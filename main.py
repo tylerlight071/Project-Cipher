67f6261b91c6677263a6d86dc8c7899194bbb133
Tyler Lightwood <tylerlightwood071@gmail.com>
2023-11-06 12:46:29 +0000

    Modified the main menu to look much better

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import time
 from IPython.display import clear_output
 
 
-def print_slow(text, delay=0.02):
+def print_slow(text, delay=0.01):
     for char in text:
         print(char, end='', flush=True)
         time.sleep(delay)
     print()
 
 
-# Main function to start the game
 def main():
     colorama.init()
     print_slow(Fore.GREEN + "---------------------------------------------" + Style.RESET_ALL)
@@ -44,21 +43,24 @@ def main():
 
     # Main menu loop
     while True:
-        print_slow("\nMain Menu:")
-        print_slow("Start")
-        print_slow("Exit")
+        print_slow(Fore.GREEN + "---------------------------------------------" + Style.RESET_ALL)
+        print_slow(Fore.GREEN + "|                 Main Menu                  |" + Style.RESET_ALL)
+        print_slow(Fore.GREEN + "---------------------------------------------" + Style.RESET_ALL)
+        print_slow(Fore.GREEN + "| [Start]  Start the game                    |" + Style.RESET_ALL)
+        print_slow(Fore.GREEN + "| [Exit]   Exit the game                     |" + Style.RESET_ALL)
+        print_slow(Fore.GREEN + "---------------------------------------------" + Style.RESET_ALL)
 
-        choice = input("Enter your choice: ")
+        choice = input(Fore.GREEN + "\nEnter your choice: " + Style.RESET_ALL)
 
         # Start the game
         if choice.lower() == "start":
             start_game()
         # Exit the game
         elif choice.lower() == "exit":
-            print_slow("Exiting...")
+            print_slow(Fore.GREEN + "\nExiting..." + Style.RESET_ALL)
             break
         else:
-            print_slow("Invalid choice, please try again.")
+            print_slow(Fore.RED + "\nInvalid choice, please try again." + Style.RESET_ALL)
 
 
 # Function to start the game
@@ -67,19 +69,19 @@ def start_game():
 
     # Game command loop
     while True:
-        command = input("> ")
+        command = input(Fore.GREEN + "> " + Style.RESET_ALL)
 
         # Connect to the network
-        if command == "connect":
+        if command.lower() == "connect":
             connect()
         # Access the mail system
-        elif command == "mail":
+        elif command.lower() == "mail":
             mail()
         # Display help message
-        elif command == "help":
+        elif command.lower() == "help":
             help_user()
         # Return to the main menu
-        elif command == "exit":
+        elif command.lower() == "exit":
             print_slow("Returning to Main Menu...")
             break
         else:
@@ -89,24 +91,22 @@ def start_game():
 # Function to connect to the network
 def connect():
     print_slow(Fore.YELLOW + "Connecting to Enigma Corps network..." + Style.RESET_ALL)
-
     # TODO: Implement hacking gameplay
 
 
 # Function to access the mail system
 def mail():
     print_slow(Fore.BLUE + "Mail System:" + Style.RESET_ALL)
-
     # TODO: Implement mail system
 
 
 # Function to display help message
 def help_user():
     print_slow(Fore.MAGENTA + "Help:" + Style.RESET_ALL)
-    print_slow("Use the 'connect' command to hack into Enigma Corps network.")
-    print_slow("Use the 'mail' command to view and respond to emails from your client and other characters.")
-    print_slow("Use the 'help' command if you need assistance at any time.")
-    print_slow("Use the 'exit' command to return to the Main Menu.")
+    print_slow("[Connect] - Use the 'connect' command to hack into Enigma Corps network.")
+    print_slow("[Mail] - Use the 'mail' command to view and respond to emails from your client and other characters.")
+    print_slow("[Help] - Use the 'help' command if you need assistance at any time.")
+    print_slow("[Exit] - Use the 'exit' command to return to the Main Menu.")
 
 
 # Run the main function when the script is executed
