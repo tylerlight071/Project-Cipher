2a880f4df2b23081fe1e14218161741a4d7e8fe5
Tyler Lightwood <tylerlightwood071@gmail.com>
2023-11-15 22:50:46 +0000

    Update code_shatter_minigame.py
    Added extra lines to space, extra message and added colours to the text where needed to help understand and look better

--- minigames/code_shatter_minigame.py
+++ minigames/code_shatter_minigame.py
@@ -4,24 +4,32 @@ from colorama import Fore, Style
 def code_shatter_minigame():
     # Generate a random 5-digit number
     target = [str(random.randint(1, 9)) for _ in range(5)]
-    
+
     print("Welcome to CodeShatter!")
+    print("")
     print("Guess the 5-digit number.")
-    print("Green: Correct digit in correct position.")
-    print("Orange: Correct digit in incorrect position.")
-    print("Red: Incorrect digit.")
-    
+    print("")
+    print("The sequence can contain multiple same numbers")
+    print("")
+    print(Fore.GREEN + "Green: Correct digit in correct position." + Style.RESET_ALL)
+    print("")
+    print(Fore.YELLOW + "Orange: Correct digit in incorrect position." + Style.RESET_ALL)
+    print("")
+    print(Fore.RED + "Red: Incorrect digit." + Style.RESET_ALL)
+    print("")
+  
+
     attempts = 0
     while attempts < 10:
         # Get the user's guess
         guess = input("Enter your guess: ")
-        
+
         if len(guess) != 5 or not guess.isdigit():
             print("Invalid input. Please enter a 5-digit number.")
             continue
-        
+
         attempts += 1
-        
+
         # Check the guess against the target
         feedback = []
         for i in range(5):
@@ -31,12 +39,14 @@ def code_shatter_minigame():
                 feedback.append(Fore.YELLOW + guess[i] + Style.RESET_ALL)
             else:
                 feedback.append(Fore.RED + guess[i] + Style.RESET_ALL)
-        
+
         print("Feedback: " + " ".join(feedback))
-        
+
         # Check if the guess is correct
         if guess == "".join(target):
             print("Congratulations! You guessed the correct number!")
             break
     else:
         print("Sorry, you've run out of attempts. The correct number was " + "".join(target) + ".")
+
+code_shatter_minigame()
