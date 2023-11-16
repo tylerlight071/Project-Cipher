36b1fd140e232f0920a54e5b6cf497b80a941a0a
Tyler Lightwood <tylerlightwood071@gmail.com>
2023-11-16 22:48:18 +0000

    Update print_slow.py
    Added print slow function

--- components/print_slow/print_slow.py
+++ components/print_slow/print_slow.py
@@ -1,1 +1,6 @@
- 
+ # Makes the text have a delay of (x) when printed to screen
+def print_slow(text, delay=0.00):  # change to 0.01
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
