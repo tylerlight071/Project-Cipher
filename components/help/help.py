67ce5bd631ba8f76f9b4a8012912cd2d3f62d481
Tyler Lightwood <tylerlightwood071@gmail.com>
2023-11-17 11:58:20 +0000

    Update help.py
    Added print slow import and removed function as unnecessary

--- components/help/help.py
+++ components/help/help.py
@@ -1,14 +1,6 @@
 import time
 from colorama import Fore, Style
-
-
-# Makes the text have a delay of (x) when printed to screen
-def print_slow(text, delay=0.00):  # change to 0.01
-    for char in text:
-        print(char, end='', flush=True)
-        time.sleep(delay)
-    print()
-
+from components.print_slow.print_slow import print_slow
 
 # Function to display help message
 def help_user():
