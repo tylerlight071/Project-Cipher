506d6dc7557bc611138afcaacc6ea354b679e23d
Tyler Lightwood <tylerlightwood071@gmail.com>
2023-11-17 19:06:27 +0000

    Update common_functions.py
    Added print_slow

--- components/common_functions.py
+++ components/common_functions.py
@@ -1,2 +1,11 @@
+import time
+
+# Makes the text have a delay of (x) when printed to screen
+def print_slow(text, delay=0.00):  # change to 0.01
+    for char in text:
+        print(char, end='', flush=True)
+        time.sleep(delay)
+    print()
+
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
